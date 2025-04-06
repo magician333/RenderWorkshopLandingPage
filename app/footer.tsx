82cce97f8efbde88b51a4701cdcbc926ce97e0ea
magician333 <magician33333@163.com>
2025-04-06 19:30:56 +0800

    change slogan at footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -89,20 +89,24 @@ export default function Footer() {
     <div className="bg-white w-full pt-16 mt-32 pb-32 border-t border-opacity-60">
       <div className="container mx-auto px-4">
         {/* Increased gap between columns */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-16"> {/* Increased gap-8 to gap-12 md:gap-16 */}
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-16">
+          {" "}
+          {/* Increased gap-8 to gap-12 md:gap-16 */}
           <div className="text-center md:text-left">
             <div className="flex justify-center md:justify-start">
               <Image src="/logo/logo.svg" alt="logo" width={200} height={100} />
             </div>
             <p className="font-medium text-xs opacity-75 mt-2">
-              Multi-Device, Distributed, Batch Rendering System
+              A distributed rendering system meticulously designed specifically
+              for Blender
             </p>
           </div>
-
           <div className="text-center md:text-left">
             {/* Added bottom margin to title */}
             <p className="font-semibold text-lg mb-3">Other Opus</p>
-            <div className="space-y-1 mt-2"> {/* Added mt-2 for spacing */}
+            <div className="space-y-1 mt-2">
+              {" "}
+              {/* Added mt-2 for spacing */}
               {opus.map((item) => (
                 <Link href={item.url} key={item.name}>
                   <p className="font-medium text-sm hover:opacity-100">
@@ -112,7 +116,6 @@ export default function Footer() {
               ))}
             </div>
           </div>
-
           <div className="text-center md:text-left">
             {/* Added bottom margin to title */}
             <p className="font-semibold text-lg mb-3">Follow Me</p>
