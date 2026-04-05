58fc67716bca181f5bc44e01727f22b8d7d65a56
purplefire <magician33333@163.com>
2026-04-05 22:48:44 +0800

    Optimized website content

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true,
+    
+  },
+  outputFileTracingIncludes: {
+    "/**": ["./content/**/*"],
   },
 };
 
